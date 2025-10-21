9de814cbcee5bdfe6974b5b330d306bdaa160bd0
Anna Mathieu <annamathieupro@gmail.com>
2025-10-21 11:48:02 +0200

    commit

---
+++ data_import.R
@@ -0,0 +1,40 @@
+# Importation des données 
+
+library(tidyverse)
+library(magick)
+
+path <- "C:\documents\Agrocampus\M2\Conference\Machine-Learning-Conference\Data\archive"
+extensions <- c("png", "jpg", "jpeg") # Formats d'images pris en charge
+
+# Lister tous les fichiers correspondants dans les sous-dossiers
+images <- list.files(
+  path = path,
+  pattern = paste0("\\.(", paste(extensions, collapse="|"), ")$"),
+  recursive = TRUE,
+  full.names = TRUE
+)
+
+# Extraire la "classe" (nom du dossier parent)
+classes <- basename(dirname(image_files)) # fresh / rotten 
+
+# Créer le data frame
+df_images <- data.frame(
+  x = images,
+  y = classes,
+  stringsAsFactors = FALSE
+)
+
+# Afficher un aperçu
+head(df_images)
+
+
+
+
+# Exportation du data set 
+write.csv(x = frui, file = "monFichier.csv")
+
+
+
+
+
+
