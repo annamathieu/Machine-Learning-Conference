a394bb9b195dca2257332eabd7cfe331f0ef16bb
Anna Mathieu <annamathieupro@gmail.com>
2025-10-21 11:47:36 +0200

    go

---
+++ starting.R
@@ -0,0 +1,12 @@
+install.packages("keras3")
+library(keras3)
+library(tensorflow)
+install_keras()
+
+require("tensorflow")
+# Check TensorFlow version
+tensorflow::tf_config()
+
+# Verify Keras setup
+keras_version()
+??keras3
